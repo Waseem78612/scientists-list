65cc5ed4aa4a017766a7c2f6715254100036d39f
Waseem Sajjad <waseem2gk@gmail.com>
2026-01-30 01:26:39 +0500

    add some new file changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets', // Default is 'assets', not a custom directory
+    outDir: 'dist', // Default output directory
+  }
 })
